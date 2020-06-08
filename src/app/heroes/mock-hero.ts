0909bcf542b4bc9c1e3e829e78e07105e54ae7d6
Minexora <12542032.giray.akkaya@gmail.com>
2020-06-08 14:02:58 +0300

    First Angular Project

---
+++ src/app/heroes/mock-hero.ts
@@ -0,0 +1,8 @@
+import { Hero } from '../heroes/hero';
+
+export const Heroes : Hero[] = [
+    {id:10, name:'Giray'}, 
+    {id:11, name:'Dr Nice'}, 
+    {id:12, name:'Narco'}, 
+    {id:13, name:'Tornado'}
+]
